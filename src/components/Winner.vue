948f82c2912d243dc8ad7eea208530cad8de235f
brianboyko <brian.boyko@gmail.com>
2020-09-27 14:28:20 +0100

    fixed issue with winner

--- src/components/Winner.vue
+++ src/components/Winner.vue
@@ -1,7 +1,21 @@
 <template>
-  <img src="@/assets/horne-big.jpg" v-if="winner === 'HORNE'" />
-  <img src="@/assets/watson-big.jpg" v-if="winner === 'WATSON'" />
-  <img src="@/assets/key-big.jpg" v-if="winner === 'KEY'" />
+  <div class="winner-container">
+    <img
+      class="winner-image"
+      src="@/assets/horne-big.jpg"
+      v-if="winner === 'HORNE'"
+    />
+    <img
+      class="winner-image"
+      src="@/assets/watson-big.jpg"
+      v-if="winner === 'WATSON'"
+    />
+    <img
+      class="winner-image"
+      src="@/assets/key-big.jpg"
+      v-if="winner === 'KEY'"
+    />
+  </div>
 </template>
 
 <script lang="ts">
@@ -16,5 +30,16 @@ export default defineComponent({
 <style lang="scss" scoped>
 img {
   max-width: 100%;
+}
+.winner-image {
+  object-fit: contain;
+  height: 50vh;
+}
+.winner-container {
+  max-height: 50vh;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
